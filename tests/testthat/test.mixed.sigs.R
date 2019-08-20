a52b7bd9f02895bdeee5a00cf1ee9e014fcd0008
steverozen <steve@steverozen.net>
2019-08-20 18:05:26 +0800

    documentation and test update

--- tests/testthat/test.mixed.sigs.R
+++ tests/testthat/test.mixed.sigs.R
@@ -1,24 +1,7 @@
-context("Test create synthetic catalogs representing several tumor types")
+context("Test CreateFromReal")
 
-test_that("CreateMixedTumorTypeSyntheticData", {
-  skip("COMPOSITE catalog not yet handled. Still Illegal.")
-  load("mixed.types.Rdata")
-  set.seed(191906)
-  num.syn.tumors <- 10
-  cancer.types <- c("Bladder-TCC", "Skin-Melanoma")
-  expect_equal(
-    CreateMixedTumorTypeSyntheticData(
-      top.level.dir = "./Bladder-Melanoma-test",
-      cancer.type.strings = cancer.types,
-      num.syn.tumors = num.syn.tumors,
-      overwrite = TRUE
-    ),
-    mixed.types)
-
-    if (TRUE) # Set this to FALSE to inspect the file and directory output
-  {
-    # Clean up
-    unlink("./Bladder-Melanoma-test",
-           recursive = TRUE, force = TRUE)
-  }
+test_that("CreateFromReal RCCOvary1000", {
+  expect_identical(
+    RCCOvary1000(regress.dir = "rdata/rcc.etc/", unlink = TRUE),
+    TRUE)
 })
--- tests/testthat/test.mutational_signature_similarity.R
+++ tests/testthat/test.mutational_signature_similarity.R
@@ -1,7 +1,7 @@
 context("Mutational signature matching")
 
 test_that("MatchSigs2Directions", {
-  load("test.data.for.matchsigs.Rdata")
+  load("data.for.matchsigs.Rdata")
   expect_equal(
     MatchSigs2Directions(.test.extracted.sigs, .test.ground.truth.sigs),
     .test.match.out)
