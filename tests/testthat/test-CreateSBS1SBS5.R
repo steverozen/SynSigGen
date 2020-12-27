04a8de6d8a5cd1b72850a09c67f95c7e41d260c6
Wu,Yang <32476254+WuyangFF95@users.noreply.github.com>
2020-12-27 10:42:12 +0800

    The results on macOS has a very slight difference (12th decimal place) compared to results on Windows. This difference is smaller than the default tolerance of all.equal (sqrt(.Machine$double.eps)), therefore we skip this test unless user want to run manually.
    This test will pass on Windows and will fail on MacOS and Linux.

--- tests/testthat/test-CreateSBS1SBS5.R
+++ tests/testthat/test-CreateSBS1SBS5.R
@@ -1,16 +1,27 @@
 context("Test Create SBS1+SBS5 correlated datasets.")
 
 test_that("CreateSBS1SBS5CorrelatedSyntheticData", {
-  skip_if_not_installed("ICAMS", minimum_version = "2.0.9")
 
-  tmp.top.level.dir <- "./tmp.SBS1.5/"
+  run.manually <- FALSE # If FALSE then skipped
+  if (run.manually) {
+    skip_if_not_installed("ICAMS", minimum_version = "2.0.9")
 
-  expect_true(
-    CreateSBS1SBS5CorrelatedSyntheticData(
-      top.level.dir = "./tmp.SBS1.5/",
-      regress.dir = "rdata/SBS1.5/",
-      overwrite = FALSE,
-      add.info = FALSE,
-      unlink = TRUE)
-  )
+    tmp.top.level.dir <- "./tmp.SBS1.5/"
+
+    expect_true(
+      CreateSBS1SBS5CorrelatedSyntheticData(
+        top.level.dir = "./tmp.SBS1.5/",
+        regress.dir = "rdata/SBS1.5/",
+        overwrite = FALSE,
+        add.info = FALSE,
+        unlink = TRUE)
+    )
+  } else {
+    skip(
+      paste0("Run manully with devtools::test(filter = \"CreateSBS1SBS5\"): ",
+            "excessively fragile test because of reliance on diff. \n",
+            "Generated exposures of different system may differ at the 12th decimal place, ",
+            "yet it does not affect the values for R core because this difference is smaller than eps."))
+
+  }
 })
