40a6788e67e421b6f2a493a769e455360c7fb7ad
jnh01 <44010028+jnh01@users.noreply.github.com>
2021-04-29 09:35:55 +0800

    Update test-GenerateSyntheticTumors.R

--- tests/testthat/test-GenerateSyntheticTumors.R
+++ tests/testthat/test-GenerateSyntheticTumors.R
@@ -10,6 +10,8 @@ test_that("Generate synthetic exposures for DBS78 using log-normal distribution"
                                exp = synthetic.exposures.DBS78,
                                my.dir = file.path(tempdir(), "DBS78"),
                                overwrite = TRUE)
+  expect_equal(dim(out), c(78, 10))
+  unlink(file.path(tempdir(), "DBS78"), recursive = TRUE)
 
 }
 )
@@ -28,6 +30,8 @@ test_that("Generate synthetic exposures for DBS78 using negative binomial distri
                                exp = synthetic.exposures.DBS78.neg.binom,
                                my.dir = file.path(tempdir(), "DBS78.neg.binom"),
                                overwrite = TRUE)
+  expect_equal(dim(out), c(78, 10))
+  unlink(file.path(tempdir(), "DBS78.neg.binom"), recursive = TRUE)
 
 }
 )
@@ -46,6 +50,9 @@ test_that("Generate synthetic tumors for DBS78 by cancer types using log-normal 
                             real.exposures = real.exposures.DBS78,
                             sample.prefix.name = "SP.Syn."
     )
+  expect_equal(dim(DBS78.synthetic.tumors$ground.truth.catalog),
+               c(78, 150))
+  unlink(file.path(tempdir(), "DBS78.synthetic.tumors"), recursive = TRUE)
 
 }
 )
@@ -54,7 +61,7 @@ test_that("Generate synthetic tumors for DBS78 by cancer types using negative bi
   # Generate synthetic tumors for DBS78
   input.sigs.DBS78 <- PCAWG7::signature$genome$DBS78
   real.exposures.DBS78 <- PCAWG7::exposure$PCAWG$DBS78
-  cancer.types <- PCAWG7::CancerTypes()[6]
+  cancer.types <- PCAWG7::CancerTypes()[1:5]
   DBS78.synthetic.tumors <-
     GenerateSyntheticTumors(seed = 191906,
                             dir = file.path(tempdir(), "DBS78.synthetic.tumors.neg.binom"),
@@ -65,8 +72,52 @@ test_that("Generate synthetic tumors for DBS78 by cancer types using negative bi
                             distribution = "neg.binom",
                             sample.prefix.name = "SP.Syn."
     )
+  expect_equal(dim(DBS78.synthetic.tumors$ground.truth.catalog),
+               c(78, 150))
+  unlink(file.path(tempdir(), "DBS78.synthetic.tumors.neg.binom"), recursive = TRUE)
 
 }
 )
 
+test_that("Generate synthetic tumors for ID by cancer types using log-normal distribution", {
+  # Generate synthetic tumors for ID
+  input.sigs.ID <- PCAWG7::signature$genome$ID
+  real.exposures.ID <- PCAWG7::exposure$PCAWG$ID
+  cancer.types <- PCAWG7::CancerTypes()[1:5]
+  ID.synthetic.tumors <-
+    GenerateSyntheticTumors(seed = 191906,
+                            dir = file.path(tempdir(), "ID.synthetic.tumors"),
+                            cancer.types = cancer.types,
+                            samples.per.cancer.type = 30,
+                            input.sigs = input.sigs.ID,
+                            real.exposures = real.exposures.ID,
+                            sample.prefix.name = "SP.Syn."
+    )
+  expect_equal(dim(ID.synthetic.tumors$ground.truth.catalog),
+               c(83, 150))
+  unlink(file.path(tempdir(), "ID.synthetic.tumors"), recursive = TRUE)
 
+}
+)
+
+test_that("Generate synthetic tumors for ID by cancer types using negative binomial distribution", {
+  # Generate synthetic tumors for ID
+  input.sigs.ID <- PCAWG7::signature$genome$ID
+  real.exposures.ID <- PCAWG7::exposure$PCAWG$ID
+  cancer.types <- PCAWG7::CancerTypes()[1:5]
+  ID.synthetic.tumors <-
+    GenerateSyntheticTumors(seed = 191906,
+                            dir = file.path(tempdir(), "ID.synthetic.tumors.neg.binom"),
+                            cancer.types = cancer.types,
+                            samples.per.cancer.type = 30,
+                            input.sigs = input.sigs.ID,
+                            real.exposures = real.exposures.ID,
+                            distribution = "neg.binom",
+                            sample.prefix.name = "SP.Syn."
+    )
+  expect_equal(dim(ID.synthetic.tumors$ground.truth.catalog),
+               c(83, 150))
+  unlink(file.path(tempdir(), "ID.synthetic.tumors.neg.binom"), recursive = TRUE)
+
+}
+)
